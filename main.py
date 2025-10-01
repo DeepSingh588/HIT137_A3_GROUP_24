4de5233835e53afa7480cc73a2392f9c1e53154d
Jagdeep Singh <worthysingh12@gmail.com>
2025-10-01 20:02:43 +0930

    Jagdeep Singh

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
  #Jagdeep Singh: Setting up the application entry point.
+from gui_app import AIApplicationWindow
+
+# The standard entry point to run the application
+if __name__ == "__main__":
+    app = AIApplicationWindow()
+    app.mainloop()
