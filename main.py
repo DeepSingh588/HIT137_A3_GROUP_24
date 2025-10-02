97b3ef0ab1732e09bed6208c0dd5604c4c6401be
Jagdeep Singh <worthysingh12@gmail.com>
2025-10-02 16:18:27 +0930

    Final Code for main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
- #Jagdeep Singh: Setting up the application entry point.
+#HIT137_A3_S2 : Group No : 24
+#Author: Jagdeep Singh: Setting up the application entry point.
+# Purpose: Main entry point for the application.
+
 from gui_app import AIApplicationWindow
 
-# The standard entry point to run the application
+# Standard check to ensure the code only runs when directly executed
 if __name__ == "__main__":
+    # Create the main window instance, which handles model initialization
     app = AIApplicationWindow()
+    
+    # Start the Tkinter event loop to display the GUI and handle all interactions
     app.mainloop()
